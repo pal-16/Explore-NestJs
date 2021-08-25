c149f628bd4d936359fc637d656852d18fdd3814
pal-16 <50794716+pal-16@users.noreply.github.com>
2021-08-25 15:31:54 +0530

    added JWT auth and login mechanism

---
+++ src/auth/service/auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Observable, from, of } from 'rxjs';
+import { User } from 'src/user/models/user.interface';
+const bcrypt = require('bcrypt');
+
+@Injectable()
+export class AuthService {
+
+    constructor(private readonly jwtService: JwtService) { }
+
+    comparePasswords(newPassword: string, passwortHash: string): Observable<any | boolean> {
+        return of<any | boolean>(bcrypt.compare(newPassword, passwortHash));
+    }
+
+    hashPassword(password: string): Observable<string> {
+        return from<string>(bcrypt.hash(password, 12));
+    }
+
+    generateJWT(user: User): Observable<string> {
+        return from(this.jwtService.signAsync({ user }));
+    }
+
+
+
+
+}
